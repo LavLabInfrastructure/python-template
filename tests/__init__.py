2528aa077e6dd5e0a927926bd728130ad0d08b54
barrettMCW <mjbarrett@mcw.edu>
2024-04-03 14:12:49 +0000

    fix pytest action failing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,9 @@
 # SPDX-FileCopyrightText: 2024-present barrettMCW <mjbarrett@mcw.edu>
 #
 # SPDX-License-Identifier: MIT
+"""example pytest module"""
+import pytest
+from src.python_template import example
+def test_example():
+    """tests pytest"""
+    assert example(True) is True
